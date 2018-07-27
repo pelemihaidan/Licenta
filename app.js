5a4968c5b455f4c75d67664396141de1790be787
Pele Mihai <pelemihaidan@gmail.com>
2018-07-27 10:22:43 +0300

    update

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./config/database');
-const request = require('request');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// Connect To Database
-mongoose.connect(config.database);
-// On Connection
-mongoose.connection.on('connected',()=>{
-    console.log('Connected to database '+config.database);
-});
-// On Connection Error
-mongoose.connection.on('error',(err)=>{
-    console.log('Database error: '+err);
-});
-
+const request = require("request");
+const WebSocket = require("ws");
 
 const app = express();
 
@@ -30,27 +18,145 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-const options = {
-    
-    url: 'https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles?count=500&price=M&granularity=M1',
-    auth: {
-        'bearer':'69577430ab5926403faac0a3055bb498-c05dde36b7a31e93661a07185ff6295e'
-    },
+let symbolList = {};
+let calendarData = {};
+let newsData = {};
+
+connect("calendar");
+function connect(p) {
+  let ws = new WebSocket("wss://ws.xapi.pro/demo");
+  // Connection to WebSocket
+  ws.on("open", () => {
+    console.log("Connected to XAPI Web Socket");
+    login();
+  });
+  ws.on("message", event => {
+    try {
+      var response = JSON.parse(event);
+      if (response.status == true) {
+        if (response.streamSessionId != undefined) {
+          // We received login response
+          if (p === "calendar") {
+            getCalendar();
+          } else if (p === "news") {
+            getNews();
+          }
+        } else {
+          if (p === "calendar") {
+            calendarData = event;
+            disconnect();
+            connect("news");
+          } else if (p === "news") {
+            newsData = event;
+            disconnect();
+            setTimeout(function() {
+              console.log("Reconnecting...");
+              connect("calendar");
+            }, 600000);
+          }
+        }
+      } else {
+        alert("Error: " + response.errorDescr);
+      }
+    } catch (Exception) {
+      alert("Fatal error while receiving data! :(");
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Connection closed");
+  });
+  function disconnect() {
+    ws.close();
+  }
+  function send(jsonMessage) {
+    try {
+      let msg = JSON.stringify(jsonMessage);
+      ws.send(msg);
+      console.log("Sent " + msg.length + " bytes of data: " + msg);
+    } catch (Exception) {
+      console.error("Error while sending data: " + Exception.message);
+    }
+  }
+  function login() {
+    let msg = {};
+    msg.command = "login";
+    let arguments = {
+        userId : 10456169,
+        password : "0c27d679"
+    };
+    msg.arguments = arguments;
+    console.log("Trying to log in as: " + msg.arguments.userId);
+    send(msg);
+  }
+  function getCalendar() {
+    let msg = {};
+    msg.command = "getCalendar";
+    console.log("Getting Calendar");
+    send(msg);
+  }
+
+  function getNews() {
+    let msg = {};
+    msg.command = "getNews";
+    let arguments = {};
+    arguments.end = 0;
+    arguments.start = 1526245794889;
+    msg.arguments = arguments;
+    console.log("Getting News");
+    send(msg);
+  }
 }
 
-// Index Route
-app.get('/', (req, res) => {
-    request(options, (error, response, body) =>{
-        if(!error && response.statusCode == 200) {
-            let info = JSON.parse(body);
-            res.send(info);
-        }  
-    })
+
+
+
+let options = {
+  url:
+    "https://api-fxpractice.oanda.com/v3/accounts/101-004-8316086-001/instruments",
+  auth: {
+    bearer: "69577430ab5926403faac0a3055bb498-c05dde36b7a31e93661a07185ff6295e"
+  }
+};
+
+app.get("/list", (req, res) => {
+  options.url ="https://api-fxpractice.oanda.com/v3/accounts/101-004-8316086-001/instruments";
+  request(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      symbolList = JSON.parse(body);
+      res.send(symbolList);
+    }
+  });
+});
+// Chart Route
+app.get("/chart", (req, res) => {
+  let symbol = req.query.symbol;
+  let granularity = req.query.granularity;
+  let from = req.query.from;
+  if (from !== " ") {
+    options.url = `https://api-fxpractice.oanda.com/v3/instruments/${symbol}/candles?count=500&price=M&from=${from}&granularity=${granularity}`;
+  } else {
+    options.url = `https://api-fxpractice.oanda.com/v3/instruments/${symbol}/candles?count=500&price=M&granularity=${granularity}`;
+  }
+  request(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      let info = JSON.parse(body);
+      res.send(info);
+    }
+  });
+});
+// Calendar Route
+app.get("/calendar", (req, res) => {
+  res.send(calendarData);
+});
+// News Route
+app.get("/news", (req, res) => {
+  res.send(newsData);
 });
 
 // Start Server
 app.listen(port, () => {
-    console.log('server started on port : ' + port );
+  console.log("server started on port : " + port);
 });
--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    database: 'mongodb://pelemihaidan:dandandan3@ds251799.mlab.com:51799/licentadb'
-};
